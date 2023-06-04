09d8752074ca2f690e3bd8356b99cd5377c7d612
emmy <52672675+emmettgb@users.noreply.github.com>
2023-06-04 06:18:58 -0600

    loader completion; new menus, new door example

--- scripts/sys/loader.gd
+++ scripts/sys/loader.gd
@@ -1,23 +1,7 @@
-extends Control
+extends CanvasLayer
 
-var next_level = null
-var root = null
-
-func load_scene(path = "res://scenes/sys/menu.tscn"):
-	$AnimationPlayer.play("fade_out")
-	var rt = get_tree().get_root()
-	rt.add_child(self)
-	next_level = path
-
-func test_load():
-	load_scene()
-
-
-func on_fade_finish(anim_name):
-	if anim_name == "fade_out":
-		get_tree().change_scene_to_file(next_level)
-		var current_scene = get_tree().get_current_scene()
-#		root.remove_child(current_scene)
-#		current_scene.call_deferred("free")
-#		next_level.add_child(self)
-#		$AnimationPlayer.play("fade_in")
+func change_scene(name):
+	$anims.play("fade_out")
+	await $anims.animation_finished
+	get_tree().change_scene_to_file(name)
+	$anims.play_backwards("fade_out")
