082ade45fba1406261b461bb02a9e28f90211908
emmy <52672675+emmettgb@users.noreply.github.com>
2023-06-04 03:27:23 -0600

    menu + some loader

--- scripts/sys/loader.gd
+++ scripts/sys/loader.gd
@@ -1,13 +1,23 @@
 extends Control
 
+var next_level = null
+var root = null
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func load_scene(path = "res://scenes/sys/menu.tscn"):
+	$AnimationPlayer.play("fade_out")
+	var rt = get_tree().get_root()
+	rt.add_child(self)
+	next_level = path
 
-func load_scene():
-	
+func test_load():
+	load_scene()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+
+func on_fade_finish(anim_name):
+	if anim_name == "fade_out":
+		get_tree().change_scene_to_file(next_level)
+		var current_scene = get_tree().get_current_scene()
+#		root.remove_child(current_scene)
+#		current_scene.call_deferred("free")
+#		next_level.add_child(self)
+#		$AnimationPlayer.play("fade_in")
